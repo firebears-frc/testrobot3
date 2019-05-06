e1fda03338b20c13890e340283d1568994f60570
Keith Rieck <KeithRieck@gmail.com>
2019-05-05 21:35:49 -0500

    Added JNI libraries for windows

--- src/test/java/frc/robot/subsystems/ShooterSubsystemTest.java
+++ src/test/java/frc/robot/subsystems/ShooterSubsystemTest.java
@@ -9,17 +9,34 @@ import org.junit.*;
 
 public class ShooterSubsystemTest {
 
+    public static final double DELTA = 0.001;
+
+    @Test
+    public void testConstructor() {
+        // Arrange
+        SpeedController motor = mock(SpeedController.class);
+        Servo servo = mock(Servo.class);
+
+        // Act
+        ShooterSubsystem shooter = new ShooterSubsystem(motor, servo);
+
+        // Assert
+        assertEquals(true, shooter.servoRetracted);
+    }
+
     @Test
     public void testSetSpeed() {
         // Arrange
         SpeedController motor = mock(SpeedController.class);
         Servo servo = mock(Servo.class);
+        when(motor.get()).thenReturn(0.5);
+
         ShooterSubsystem shooter = new ShooterSubsystem(motor, servo);
 
         // Act
         shooter.setSpeed(0.5);
 
         // Assert
-        assertEquals(0.5, shooter.shooterMotor.get());
+        assertEquals(0.5, shooter.shooterMotor.get(), DELTA);
     }
 }
